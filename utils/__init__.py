0f3fb0ddf7e11c60701e6d82ec4c3907f5e55277
Michail Semoglou <101779823+MichailSemoglou@users.noreply.github.com>
2025-03-11 18:17:27 +0800

    Delete utils directory

--- utils/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Utility modules for the Color Palette Extractor.
-"""
-
-from .color import rgb_to_cmyk, cmyk_to_rgb, hex_to_rgb, rgb_to_hex, get_contrast_color
-from .image import is_valid_image, find_images_in_directory, resize_for_processing
-from .cache import CacheManager
