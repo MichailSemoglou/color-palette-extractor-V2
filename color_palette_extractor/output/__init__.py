1919fcf82f9389c6ad42808e1791dfb341060c7a
Michail Semoglou <101779823+MichailSemoglou@users.noreply.github.com>
2025-05-21 12:04:29 +0800

    Update __init__.py

--- color_palette_extractor/output/__init__.py
+++ color_palette_extractor/output/__init__.py
@@ -4,3 +4,4 @@ Output generation modules for the Color Palette Extractor 2.0
 
 from .pdf import save_palette_to_pdf
 from .text import save_palette_and_harmonies
+from .emotional import save_emotional_analysis
